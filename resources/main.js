d782f8e779eb3a705600838ae6e3f5a4f7c02def
Marco Bernasocchi <marco@opengis.ch>
2024-02-19 00:15:18 +0100

    add loading screen and better sizing

--- resources/main.js
+++ resources/main.js
@@ -6,7 +6,20 @@ Cesium.Ion.defaultAccessToken =
 const viewer = new Cesium.Viewer("cesiumContainer", {
   terrain: Cesium.Terrain.fromWorldTerrain(),
 });
+window.viewer = viewer;
 viewer.clock.multiplier = 120;
+
+
+// init UI
+// Move HTML buttons to Cesium toolbar
+const toolbar = document.querySelector("div.cesium-viewer-toolbar");
+const modeButton = document.querySelector("span.cesium-sceneModePicker-wrapper");
+const buttons = document.getElementById("buttons").children;
+for (var i = 0; i < buttons.length; i++) {
+  var button = buttons[i];
+  toolbar.insertBefore(button, modeButton);
+};
+
 
 // add DragDropMixin to the viewer
 viewer.extend(Cesium.viewerDragDropMixin, {
@@ -16,7 +29,7 @@ viewer.extend(Cesium.viewerDragDropMixin, {
 });
 
 // add eventListener to html-input-Element with type="file"
-document.getElementById("gpxfile").addEventListener("change", loadFile, false);
+document.getElementById("fileupload").addEventListener("change", loadFile, false);
 
 // fly to the added dataSource
 viewer.dataSources.dataSourceAdded.addEventListener(function (source) {
@@ -34,3 +47,6 @@ function loadFile(event) {
     })
   );
 }
+
+
+document.body.classList.remove("cesium-loading");
